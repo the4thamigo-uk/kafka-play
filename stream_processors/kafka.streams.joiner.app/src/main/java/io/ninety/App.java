6046707aa3a654fd79ef24f401b8535da4c3874e
Andy Smith <andy.smith@90poe.io>
2019-03-01 16:34:40 +0000

    Temporary commit

--- stream_processors/kafka.streams.joiner.app/src/main/java/io/ninety/App.java
+++ stream_processors/kafka.streams.joiner.app/src/main/java/io/ninety/App.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.ninety;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -24,50 +8,51 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import org.apache.avro.generic.GenericRecord;
 
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-/**
- * Demonstrates, using the high-level KStream DSL, how to implement the WordCount program
- * that computes a simple word occurrence histogram from an input text.
- * <p>
- * In this example, the input stream reads from a topic named "streams-plaintext-input", where the values of messages
- * represent lines of text; and the histogram output is written to topic "streams-wordcount-output" where each record
- * is an updated count of a single word.
- * <p>
- * Before running this example you must create the input topic and the output topic (e.g. via
- * {@code bin/kafka-topics.sh --create ...}), and write some data to the input topic (e.g. via
- * {@code bin/kafka-console-producer.sh}). Otherwise you won't see any data arriving in the output topic.
- */
-public final class App {
+public class App 
+{
+    public static void main( String[] args )
+    {
+      final Properties props = new Properties();
+        final String leftTopic = System.getenv("JOINER_LEFT_TOPIC");
+        final String rightTopic = System.getenv("JOINER_RIGHT_TOPIC");
+        final String keySerde = Serdes.String();
 
-    public static void main(final String[] args) {
-        final Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-joiner");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-
-        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
-        // Note: To re-run the demo, you need to use the offset reset tool:
-        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, keySerde.getClass().getName());
+
+        // we dont need these
+        //props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // create avro serializers/deserializers
+        final GenericAvroSerde avroSerde = new GenericAvroSerde();
+        final Map<String, String> serdeConfig = ImmutableMap.of(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, props.get(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG).toString());
+        avroSerde.configure(serdeConfig, false);
 
         final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> leftStream = builder.stream(leftTopic, Consumed.with(keySerde, avroSerde);
+        final KStream<String, String> rightStream = builder.stream(rightTopic, Consumed.with(keySerde, avroSerde);
 
-        final KStream<String, String> source = builder.stream("streams-plaintext-input");
-
-        final KTable<String, Long> counts = source
-            .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split(" ")))
-            .groupBy((key, value) -> value)
-            .count();
-
-        // need to override value serde to Long type
-        counts.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
+          final KStream<GenericRecord, GenericRecord> joinStream = leftStream.join(rightStream,
+            (left, right) -> {
+              GenericRecord joined = new GenericData.Record(schema);
+              viewRegion.put("user", view.get("user"));
+              viewRegion.put("page", view.get("page"));
+              viewRegion.put("region", region);
+              return viewRegion;            
+            },
+          JoinWindows.of(TimeUnit.MINUTES.toMillis(5)),
+        );
 
         final KafkaStreams streams = new KafkaStreams(builder.build(), props);
         final CountDownLatch latch = new CountDownLatch(1);
@@ -89,4 +74,4 @@ public final class App {
         }
         System.exit(0);
     }
-}
+d
