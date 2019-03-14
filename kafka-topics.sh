2b1ff39520660f708078a56c793a65f5bc745511
Andy Smith <andy.smith@90poe.io>
2019-03-14 18:40:30 +0000

    Create kafka-topics.sh script

--- kafka-topics.sh
+++ kafka-topics.sh
@@ -3,6 +3,4 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $script_dir/env.sh
 
-topic="$1"
-
-$script_dir/kafka-topics.sh --create --replication-factor 1 --partitions 1 --topic "$topic"
+$dc exec broker kafka-topics --zookeeper "$zookeeper_addr" "${@:1}"
