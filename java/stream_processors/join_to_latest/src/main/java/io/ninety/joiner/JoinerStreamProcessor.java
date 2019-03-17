bfd2edb197c43c3036c8d8fdea7e7c1eacb0fd7b
Andy Smith <andy.smith@90poe.io>
2019-03-17 23:39:46 +0000

    Set retention to 1 hour

--- java/stream_processors/join_to_latest/src/main/java/io/ninety/joiner/JoinerStreamProcessor.java
+++ java/stream_processors/join_to_latest/src/main/java/io/ninety/joiner/JoinerStreamProcessor.java
@@ -29,8 +29,8 @@ public class JoinerStreamProcessor {
 		props.put(JoinerProperties.AGGREGATE_TIMESTAMP_FIELD, "event_time_2");
 		props.put(JoinerProperties.JOIN_WINDOW_SIZE, "PT5S");
 		props.put(JoinerProperties.GROUP_WINDOW_SIZE, "PT5S");
-		props.put(JoinerProperties.JOIN_WINDOW_RETENTION, "PT60S");
-		props.put(JoinerProperties.GROUP_WINDOW_RETENTION, "PT60S");
+		props.put(JoinerProperties.JOIN_WINDOW_RETENTION, "PT1H");
+		props.put(JoinerProperties.GROUP_WINDOW_RETENTION, "PT1H");
 		props.put(JoinerProperties.LEFT_FIELDS, "event_time as event_time_1, key1 as key1_1, key2 as key2_1, val as val_1");
 		props.put(JoinerProperties.RIGHT_FIELDS, "event_time as event_time_2, key1 as key1_2, key2 as key2_2, val as val_2");
 
